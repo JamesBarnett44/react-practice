5c3a8ac126899ae77d4f3acc814992d0d558328e
Jamie Barnett <jamiebarnett44@gmail.com>
2021-06-30 10:12:32 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/pageComponents/Home';
+import About from './components/pageComponents/About';
+import Contact from './components/pageComponents/Contact';
+import Product from './components/pageComponents/Product';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+// Router switch case goes in parent component
+// Nav (or equivalent) goes at the top of the router
+// Footer goes at the bottom
+// Anything in <Switch> are the cases to switch between
+// Route specify path, if path matches render that component
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Switch>
+
+          <Route path="/" exact>
+            <Home />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/contact">
+            <Contact />
+          </Route>
+
+          <Route path="/product">
+            <Product />
+          </Route>
+
+        </Switch>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/pageComponents/Product.jsx
@@ -0,0 +1,7 @@
+const Product = () => {
+    return (
+        <h3> Product</h3>
+    );
+}
+
+export default Product;
